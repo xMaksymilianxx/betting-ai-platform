6f4243f25184eccf92fe77fac630b9aff01489d3
xMaksymilianxx <maksymilianr@o2.pl>
2025-10-07 18:11:39 +0200

    Delete lib directory

--- lib/ml-models/value-bet-hunter.ts
+++
@@ -1,7 +0,0 @@
-export class ValueBetHunter {
-  findValue(prediction: number, bookmakerOdds: number): number {
-    const fairOdds = 1 / prediction;
-    const edge = (fairOdds / bookmakerOdds) - 1;
-    return edge > 0.05 ? edge : 0;
-  }
-}
